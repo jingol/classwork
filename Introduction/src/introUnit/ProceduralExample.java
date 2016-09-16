b3d0ae063b5a35bdb776814f5bc7077394dc83da
ghcstking <vlau8050@bths.edu>
2016-09-16 11:53:57 -0400

    Jillian introduces herself

--- Introduction/src/introUnit/ProceduralExample.java
+++ Introduction/src/introUnit/ProceduralExample.java
@@ -10,7 +10,7 @@ package introUnit;
 public class ProceduralExample {
 	public static void main(String[] args){
 		String[] names = {"Jillian", "Joseph", "Jordan"};
-		String [] years = {"Senior", "Sophomore", "Junior"};
+		String[] years = {"Senior", "Sophomore", "Junior"};
 		System.out.println("My name is " + names[0] + "and I am a " + years[0] + ".");
 	}
 }
