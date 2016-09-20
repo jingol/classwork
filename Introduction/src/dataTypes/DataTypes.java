ada073694bc0962d89edd00704b96b8f7de903b7
ghcstking <vlau8050@bths.edu>
2016-09-20 01:02:39 -0400

    homework

--- Introduction/src/dataTypes/DataTypes.java
+++ Introduction/src/dataTypes/DataTypes.java
@@ -17,5 +17,38 @@ public class DataTypes {
 		if(42 == 42.0) {
 			System.out.println("42 equals 42.0");
 		}
+		double d1 = 4.64;
+		double d2 = 2.0;
+		double d3 = 2.64;
+		
+		System.out.println("d1 : " + d1);
+		System.out.println("d2 : " + d2); 
+		System.out.println("d3 : " + d3); 
+		System.out.println("d1 - d2 : " + (d1 - d2));
+		
+//		String s = 1;
+//		System.out.println(s);
+//		type should be int
+//		String s = (String)1;
+//		System.out.println(s);
+//		cannot cast string on int
+//		String s = "1";
+//		System.out.println(s);
+//		String s = ""+1;
+//		System.out.println(s);
+//		String s = "Happy "+18+"th birthday!";
+//		System.out.println(s);
+//		String s = "Happy 18th birthday!";
+//		System.out.println(s);
+//		String s = "Happy "+54/3+"th birthday!";
+//		System.out.println(s);
+//		String s = "Happy "+(54/3)+"th birthday!";
+//		System.out.println(s);
+//		int m = 22;
+//		int n = 7;
+//		System.out.println("m/n is "+(m/n));
+		int m = 22;
+		int n = 7;
+		System.out.println("m/n is "+((double)m/n));
 	}
 }
