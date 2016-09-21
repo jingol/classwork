a7a0890c3864d490a81d12e00000140ae2c53204
Student6 <Student6@10.8.33.107>
2016-09-21 12:55:44 -0400

    Unfair die

--- Introduction/src/dataTypes/MathRandom.java
+++ Introduction/src/dataTypes/MathRandom.java
@@ -8,15 +8,38 @@ public class MathRandom {
 	}
 
 	public static void main(String[] args) {
-		for(int index = 0; index < 10; index++) {
-			System.out.println("Roll #" + (index + 1) + " Die is " + rollFairDie());
+		int[] results = new int[6];
+		int totalRolls = 10000;
+		for(int index = 0; index < totalRolls; index++) {
+			int roll = rollUnfairDie();
+//			System.out.println("Roll #" + (index + 1) + " Die is " + roll);
+			results[roll-1]++;
 		}
-
+		for (int i= 0; i < 6; i++) {
+			int percentage = (int) Math.round((100*((double)results[i]/totalRolls)));
+			System.out.println((i+1) + " was rolled " + percentage + "%.");
+		}
 	}
 	public static int rollFairDie() {
-		Random gen = new Random();
-		return gen.nextInt(6) + 1;
+//		Random gen = new Random();
+//		return gen.nextInt(6) + 1;
+		double rand = Math.random(); // creates a random double (0,1)
+		int roll = (int) (6 * rand); //max value [0,5]
+		return roll+1; //0 becomes 1, 5 becomes 6
 	}
 	public static int rollUnfairDie() {
+		double rand = Math.random();
+		int roll = (int) (6 * rand) + 1;
+		int num = (int) (10 * Math.random()) + 1;
+		if (roll < 6 && roll > 4 && num < 5) {
+			return roll + 1;
+		}
+		else {
+			return roll;
+		}
 	}
+//	public static String pickCard() {
+//		Suits[] suits = {"Spades", "Diamonds", "Hearts", "Clubs"};
+//		Ranks[] ranks = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
+//	}
 }
