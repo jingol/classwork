590a03e1405f08a5e60dfdd3790a784e000b2be9
Student6 <Student6@10.8.33.107>
2016-09-22 12:33:39 -0400

    heheh

--- Introduction/src/dataTypes/StringPractice.java
+++ Introduction/src/dataTypes/StringPractice.java
@@ -1,24 +1,57 @@
 package dataTypes;
 
-public class StringPractice {
+import java.util.Scanner;
 
+public class StringPractice {
+	static String user;
+	static int lineCount = 0;
+	static Scanner input;
 	public static void main(String[] args) {
-//		String text = new String("Hello World");
+		createAScanner();
+		// demonstrateStringMethods();
+		promptname();
+		talkForever();
+	}
+	private static void promptname() {
+		print("Hello, human. I am a board covered with semiconductors and other such components"
+				+ ". What is your name?");
+		user = input.nextLine();
+		print("Awesome! I will call you user " + user + " until you terminate me.");
+	}
+	public static void talkForever() {
+		while(true) {
+			promptInput();
+		}
+	}
+	public static void print(String s) {
+		lineCount++;
+		System.out.println("Line #" + lineCount + ": " +s);
+	}
+	public static void promptInput() {
+		print(user + ", try inputing a string");
+		String userInput = input.nextLine();
+		print("You typed: " + userInput);
+	}
+	public static void createAScanner() {
+		input = new Scanner(System.in);
+	}
+	public static void demonstrateStringMethods() {
+		// String text = new String("Hello World");
 		String text1 = "Hello World"; //shortcut for creating strings, no need for constructor
 		String text2 = "Hello ";
 		String text3 = "World";
 		
 		if (text1.equals(text2 + text3)) {
-			System.out.println("These strings are equal.");
+			print("These strings are equal.");
 		}
 		else {
-			System.out.println(text1);
-			System.out.println(text2 + text3);
+			print(text1);
+			print(text2 + text3);
 		}
 		String word1 = "Aardvark";
 		String word2 = "Zyzzyva";
 		if (word1.compareTo(word2) < 0) {
-			System.out.println("word1 is before word2, lexicographically");
+			print("word1 comes before word2, lexicographically");
 		}
 	}
 
