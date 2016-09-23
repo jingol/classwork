df0f7fe5129541868f9f39554e7077b566108ef8
Student6 <Student6@10.8.33.107>
2016-09-23 13:18:02 -0400

    school pt 2

--- Introduction/src/chatBot/School.java
+++ Introduction/src/chatBot/School.java
@@ -10,6 +10,10 @@ public class School implements Topic {
 		while (inSchoolLoop) {
 			VickiMain.print("Tell me about school.");
 			schoolsResponse = VickiMain.getInput();
+			if (schoolsResponse.indexOf("Stop") >= 0) {
+				inSchoolLoop = false;
+				VickiMain.talkForever();
+			}
 			VickiMain.print("That's my favorite part about school too.");
 		}
 
