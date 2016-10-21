928dc909165e951f59ec385af8c36c01b9cbd3b0
Student6 <Student6@10.8.33.107>
2016-10-21 13:19:03 -0400

    sdfsdfsdf

--- Introduction/src/arrays/ArrayMethods.java
+++ Introduction/src/arrays/ArrayMethods.java
@@ -3,7 +3,8 @@ package arrays;
 public class ArrayMethods {
 	public static void main(String[] args) {
 		int[] arr = {2,3,4,9,11,12,15};
-		swap(arr, 0, arr.length-1);
+		// swap(arr, 0, arr.length-1);
+		shuffle(arr);
 		checkHalfway(arr, 12, 0, arr.length-1);
 //		if (checkHalfway(arr, 12, 0, arr.length-1)) {
 //			System.out.println("The number you are searching for is less than the value in the middle of the array");
@@ -13,17 +14,20 @@ public class ArrayMethods {
 //					+ "of the array");
 //		}
 	}
-
+	
 	public static void swap(int[] array, int first, int last) {
 		int placeholder = array[last];
 		array[last] = array[first];
 		array[first] = placeholder;
-		for (int i = 0; i < array.length; i++) {
-			System.out.println(array[i]);
-		}
 	}
 
 	public static boolean checkHalfway(int[] array, int searchValue, int begin, int end) {
 		return searchValue < array[(begin+end+1)/2];
+	}
+	public static void shuffle (int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			int random = (int)(Math.random()*array.length);
+			swap(array, i, random);
+		}
 	}
 }
